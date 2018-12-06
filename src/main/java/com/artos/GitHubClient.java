3b333aecf463d28b82d51f4401c0b52775962a12
Ludovick XIV <ludovickXIV@gmail.com>
2018-12-06 11:18:38 +0200

    Добавил комментарии

--- src/main/java/com/artos/GitHubClient.java
+++ src/main/java/com/artos/GitHubClient.java
@@ -17,9 +17,27 @@ import retrofit2.http.Query;
  * @author Ludovick XIV
  */
 public interface GitHubClient {
+    /**
+     * Для первого запроса к серверу для получения 
+     * данных о пользователе
+     * @param authHeader добавление заголовка для 
+     * базовой авторизации
+     * @return возвращает экземпляр класса User
+     */
     @GET("/user")
     Call <User> getUser(@Header("Authorization") String authHeader);
     
+    /**
+     * Для второго запроса к серверу для получения
+     * пользовательских репозиториев
+     * @param username передаём пользовательский логин
+     * получаем его из первого запроса
+     * @param type передаём возможные параметры:
+     * all - все репозитории, 
+     * owner - собственные, 
+     * member - в тех что пользователь участник;
+     * @return возвращает список репозиториев
+     */
     @GET("/users/{username}/repos")
     Call <List<Repo>> getRepo(@Path("username") String username, @Query("type") String type);
 }
